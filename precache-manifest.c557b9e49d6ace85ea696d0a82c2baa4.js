da78bdd1d626a599180c5bab7fe74cd1e65b29c6
ScriptCamilo <script.camilo@gmail.com>
2022-09-05 22:29:35 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c557b9e49d6ace85ea696d0a82c2baa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378fe4f9bd85e9279425700ca589d9dc",
+    "url": "/trybe-react-testing-library/index.html"
+  },
+  {
+    "revision": "5a50eedd3f5ba783361c",
+    "url": "/trybe-react-testing-library/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/trybe-react-testing-library/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trybe-react-testing-library/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a50eedd3f5ba783361c",
+    "url": "/trybe-react-testing-library/static/js/main.c30f8de5.chunk.js"
+  },
+  {
+    "revision": "3a704167b8bb725766d1",
+    "url": "/trybe-react-testing-library/static/js/runtime-main.37dfcab9.js"
+  }
+]);
